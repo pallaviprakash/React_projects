da4210191cf97af350d061a6552a77bab0d98d3f
pallaviprakash <pallavi@targetclose.com>
2021-04-02 22:53:22 +0530

    React_Module2
    Module 2 example started

---
+++ src/component/layout/MainNavigation.js
@@ -0,0 +1,26 @@
+import {useContext} from 'react';
+import { Link } from 'react-router-dom'
+import classes from './MainNavigation.module.css'
+import FavouritesContext from '../../store/Favourites-contxt';
+
+function MainNavigation(){
+    const favouriteCtx = useContext(FavouritesContext);
+
+    return ( <header className='classes.header'>
+        <div>
+            React Meetups
+        </div>
+        <nav>
+            <ul>
+                <li><Link to='/'>All Meetups</Link></li>
+                <li><Link to='/new-meetup'>Add New Meetup</Link></li>
+                <li><Link to='/Favourites'>
+                    My Favourites
+                    <span className={classes.badge}>{favouriteCtx.totalFavourites}</span>
+                    </Link></li>
+            </ul>
+        </nav>
+    </header>
+    );
+}
+export default MainNavigation;
